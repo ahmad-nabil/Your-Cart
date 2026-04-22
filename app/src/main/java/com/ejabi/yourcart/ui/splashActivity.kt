d63812bd959943a43d4f289caadbcdca9f6579be
Android_DEV <ahmadnabil@bk.ru>
2026-04-22 11:41:06 +0300

    auto animate

--- app/src/main/java/com/ejabi/yourcart/ui/splashActivity.kt
+++ app/src/main/java/com/ejabi/yourcart/ui/splashActivity.kt
@@ -1,35 +1,53 @@
 package com.ejabi.yourcart.ui
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
+import com.ejabi.yourcart.R
 import com.ejabi.yourcart.databinding.ActivitySplashBinding
 import com.ejabi.yourcart.ui.login.LoginActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class splashActivity : AppCompatActivity() {
-lateinit var  binding: ActivitySplashBinding
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-binding= ActivitySplashBinding.inflate(layoutInflater)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.motionLayout.transitionToEnd()
-        binding.motionLayout.postDelayed({
-            startActivity(Intent(this@splashActivity, LoginActivity::class.java))
-            finish()
-        }, 10000)
+        binding.motionLayout.setTransition(R.id.splash_transition)
+
         binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionStarted(layout: MotionLayout, startId: Int, endId: Int) {}
-            override fun onTransitionChange(layout: MotionLayout, startId: Int, endId: Int, progress: Float) {}
+
+            override fun onTransitionChange(
+                layout: MotionLayout,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {}
 
             override fun onTransitionCompleted(layout: MotionLayout, currentId: Int) {
-                layout.postDelayed({
-                    startActivity(Intent(this@splashActivity, LoginActivity::class.java))
-                    finish()
-                }, 10000)
+                startActivity(Intent(this@splashActivity, LoginActivity::class.java))
+                finish()
             }
 
-            override fun onTransitionTrigger(layout: MotionLayout, triggerId: Int, positive: Boolean, progress: Float) {}
+            override fun onTransitionTrigger(
+                layout: MotionLayout,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {}
         })
+
+        binding.motionLayout.post {
+            binding.motionLayout.progress = 0f
+            binding.motionLayout.transitionToEnd()
+        }
     }
 }
